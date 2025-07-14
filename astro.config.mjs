16504964411718b6286ac51a2673365c5db7a157
InfiniteCoder <info@infinitecoder.org>
2025-07-14 12:30:04 +0300

    Update website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import mdx from '@astrojs/mdx';
-
-import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.infinitecoder.org',
-  integrations: [mdx(), icon()]
+  integrations: [mdx()]
 });
