5e869f6cf99028ef6efab12f8d961731d9a90d26
InfiniteCoder <info@infinitecoder.org>
2026-01-03 18:43:48 +0300

    Update the website

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.infinitecoder.org',
+  build: { inlineStylesheets: 'always' },
   integrations: [mdx()]
 });
